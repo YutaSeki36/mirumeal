77f77ca1eb67bf872f75d3dabd4a1eb3dcf8d037
YutaSeki36 <sekiyuta0306@gmail.com>
2017-02-10 22:37:17 +0900

    円グラフ追加

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,31 @@ var express = require('express');
 var router = express.Router();
 var moment = require('moment');
 var pg = require('pg');
+var ChartjsNode = require('chartjs-node');
 
 /* GET home page. */
 router.get('/', function(request, response, next) {
     var con = "tcp://sekiyuuta:root@localhost:5432/postgres"; //
     pg.connect(con, function(err, client) {
        var query = 'SELECT * FROM border';
+       var chart1 = 'select count(*) from logeat where status_id= ' + "'"+'自炊'+"'";
+       var chart2 = 'select count(*) from logeat where status_id= ' + "'"+'カップラーメン'+"'";
+       var chart3 = 'select count(*) from logeat where status_id= ' + "'"+'外食'+"'";
           client.query(query, function(err, query0) {
-             response.render('index', {
-               title: 'Express',
-               borderList: query0.rows
-             });
-            client.end();//end();で終わらせないと無限にセッションを作られる
+            client.query(chart1,function(err,_chart1){
+              client.query(chart2,function(err,_chart2){
+                client.query(chart3,function(err,_chart3){
+                  console.log(_chart1.rows[0].count);
+                  response.render('index', {
+                    title: 'Express',
+                    chart1: _chart1.rows[0].count,
+                    chart2: _chart2.rows[0].count,
+                    chart3: _chart3.rows[0].count,
+                    borderList: query0.rows
+                  });
+                });
+              });
+            });
           });
        pg.end();
     });
