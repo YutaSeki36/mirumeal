812db46ed3b456d266f292734e0e4b000beb1cc2
YutaSeki36 <sekiyuta0306@gmail.com>
2017-02-11 06:51:32 +0900

    proc編集

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var pg = require('pg');
 
 /* GET home page. */
 router.get('/', function(request, response, next) {
-    var con = "tcp://postgres:root@localhost:5432/postgres"; //
+    var con = "tcp://gyqxwlnruqdage:50b52d08b756769692b56e29f39dcfe0056c8d73814d92f359e96976833e9da4@ec2-184-72-246-219.compute-1.amazonaws.com:5432/df502fvkn3s7gn"; //
     pg.connect(con, function(err, client) {
        var query = 'SELECT * FROM border';
        var chart1 = 'select count(*) from logeat where status_id= ' + "'"+'自炊'+"'";
@@ -54,7 +54,7 @@ router.post('/', function(request, response, next) {
     var log_eat=request.body["log_date"];
     var title=log_eat+'の食事データ';
     console.log(title);
-    var con = "tcp://postgres:root@localhost:5432/postgres";
+    var con = "tcp://gyqxwlnruqdage:50b52d08b756769692b56e29f39dcfe0056c8d73814d92f359e96976833e9da4@ec2-184-72-246-219.compute-1.amazonaws.com:5432/df502fvkn3s7gn";
     pg.connect(con, function(err, client) {
         var qstr = "insert into border (title, created_at) VALUES($1, $2);";
         var query = client.query(qstr,[title,log_eat]);
